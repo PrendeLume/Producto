055adc5dc98c319f9a2764f09072a1ab92a7fcd7
Rgcenteno <rgcenteno@edu.xunta.gal>
2022-02-18 13:37:16 +0100

    Cambios en BBDD

--- demoUD4.sql
+++ demoUD4.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:3306
--- Xerado en: 07 de Xan de 2022 ás 14:40
--- Versión do servidor: 10.3.32-MariaDB-0ubuntu0.20.04.1
--- Versión do PHP: 7.4.3
+-- Tiempo de generación: 18-02-2022 a las 13:35:02
+-- Versión del servidor: 10.3.32-MariaDB-0ubuntu0.20.04.1
+-- Versión de PHP: 7.4.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,7 +25,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura da táboa `categoria`
+-- Estructura de tabla para la tabla `categoria`
 --
 
 CREATE TABLE `categoria` (
@@ -35,14 +35,13 @@ CREATE TABLE `categoria` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- A extraer os datos da táboa `categoria`
+-- Volcado de datos para la tabla `categoria`
 --
 
 INSERT INTO `categoria` (`id_categoria`, `nombre_categoria`, `id_padre`) VALUES
 (3, 'Videojuegos', NULL),
 (4, 'Microsoft', 3),
 (5, 'Nintendo', 3),
-(6, 'Xbox Series S/X', 4),
 (7, 'Sony', 3),
 (13, 'Telefonía', NULL),
 (14, 'Android', 13),
@@ -51,7 +50,7 @@ INSERT INTO `categoria` (`id_categoria`, `nombre_categoria`, `id_padre`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estrutura da táboa `log`
+-- Estructura de tabla para la tabla `log`
 --
 
 CREATE TABLE `log` (
@@ -62,7 +61,7 @@ CREATE TABLE `log` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- A extraer os datos da táboa `log`
+-- Volcado de datos para la tabla `log`
 --
 
 INSERT INTO `log` (`id_log`, `operacion`, `tabla`, `detalle`) VALUES
@@ -71,7 +70,148 @@ INSERT INTO `log` (`id_log`, `operacion`, `tabla`, `detalle`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estrutura da táboa `usuario`
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `codigo` varchar(10) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `descripcion` text NOT NULL,
+  `proveedor` varchar(9) NOT NULL,
+  `coste` decimal(10,0) NOT NULL,
+  `margen` decimal(10,0) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `iva` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proveedor`
+--
+
+CREATE TABLE `proveedor` (
+  `cif` varchar(9) NOT NULL,
+  `codigo` varchar(10) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `direccion` varchar(255) NOT NULL,
+  `website` varchar(255) NOT NULL,
+  `pais` varchar(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `telefono` varchar(12) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `proveedor`
+--
+
+INSERT INTO `proveedor` (`cif`, `codigo`, `nombre`, `direccion`, `website`, `pais`, `email`, `telefono`) VALUES
+('A5000002E', 'test', 'test', 'test', 'http://test.org', 'Portugal', 'test@test.org', '999999999'),
+('A5000003E', 'test', 'test', 'test', 'http://test.org', 'Portugal', 'test@test.org', '999999999'),
+('A5000031E', 'PROV000031', 'PROVEEDOR 000031', 'Calle Test Num. 581', 'http://test.org', 'España', 'proveedor30@test.org', NULL),
+('A5009999E', 'test', 'test', 'test', 'http://test.org', 'Portugal', 'test@test.org', NULL),
+('A6000061I', 'PROV000061', 'PROVEEDOR 000061', 'Calle Test Num. 321', 'http://test.org', 'España', 'proveedor60@test.org', NULL),
+('A6000071S', 'PROV000071', 'PROVEEDOR 000071', 'Calle Test Num. 382', 'http://test.org', 'España', 'proveedor70@test.org', NULL),
+('A6000081C', 'PROV000081', 'PROVEEDOR 000081', 'Calle Test Num. 81', 'http://test.org', 'España', 'proveedor80@test.org', NULL),
+('A7000041O', 'PROV000041', 'PROVEEDOR 000041', 'Calle Test Num. 643', 'http://test.org', 'España', 'proveedor40@test.org', NULL),
+('A8000011K', 'PROV000011', 'PROVEEDOR 000011', 'Calle Test Num. 578', 'http://test.org', 'España', 'proveedor10@test.org', NULL),
+('A8000021U', 'PROV000021', 'PROVEEDOR 000021', 'Calle Test Num. 400', 'http://test.org', 'España', 'proveedor20@test.org', NULL),
+('A8000051Y', 'PROV000051', 'PROVEEDOR 000051', 'Calle Test Num. 367', 'http://test.org', 'España', 'proveedor50@test.org', NULL),
+('A8000091M', 'PROV000091', 'PROVEEDOR 000091', 'Calle Test Num. 727', 'http://test.org', 'España', 'proveedor90@test.org', NULL),
+('B1000062J', 'PROV000062', 'PROVEEDOR 000062', 'Calle Test Num. 265', 'http://test.org', 'España', 'proveedor61@test.org', NULL),
+('B3000092N', 'PROV000092', 'PROVEEDOR 000092', 'Calle Test Num. 825', 'http://test.org', 'España', 'proveedor91@test.org', NULL),
+('B4000052Z', 'PROV000052', 'PROVEEDOR 000052', 'Calle Test Num. 998', 'http://test.org', 'España', 'proveedor51@test.org', NULL),
+('B5000042P', 'PROV000042', 'PROVEEDOR 000042', 'Calle Test Num. 719', 'http://test.org', 'España', 'proveedor41@test.org', NULL),
+('B5000082D', 'PROV000082', 'PROVEEDOR 000082', 'Calle Test Num. 534', 'http://test.org', 'España', 'proveedor81@test.org', NULL),
+('B6000072T', 'PROV000072', 'PROVEEDOR 000072', 'Calle Test Num. 982', 'http://test.org', 'España', 'proveedor71@test.org', NULL),
+('B7000002B', 'PROV000002', 'PROVEEDOR 000002', 'Calle Test Num. 874', 'http://test.org', 'España', 'proveedor1@test.org', NULL),
+('B8000012L', 'PROV000012', 'PROVEEDOR 000012', 'Calle Test Num. 928', 'http://test.org', 'España', 'proveedor11@test.org', NULL),
+('B8000022V', 'PROV000022', 'PROVEEDOR 000022', 'Calle Test Num. 56', 'http://test.org', 'España', 'proveedor21@test.org', NULL),
+('B9000032F', 'PROV000032', 'PROVEEDOR 000032', 'Calle Test Num. 173', 'http://test.org', 'España', 'proveedor31@test.org', NULL),
+('C1000013M', 'PROV000013', 'PROVEEDOR 000013', 'Calle Test Num. 600', 'http://test.org', 'España', 'proveedor12@test.org', NULL),
+('C1000083E', 'PROV000083', 'PROVEEDOR 000083', 'Calle Test Num. 796', 'http://test.org', 'España', 'proveedor82@test.org', NULL),
+('C2000003C', 'PROV000003', 'PROVEEDOR 000003', 'Calle Test Num. 193', 'http://test.org', 'España', 'proveedor2@test.org', NULL),
+('C3000023W', 'PROV000023', 'PROVEEDOR 000023', 'Calle Test Num. 428', 'http://test.org', 'España', 'proveedor22@test.org', NULL),
+('C3000033G', 'PROV000033', 'PROVEEDOR 000033', 'Calle Test Num. 837', 'http://test.org', 'España', 'proveedor32@test.org', NULL),
+('C4000043Q', 'PROV000043', 'PROVEEDOR 000043', 'Calle Test Num. 542', 'http://test.org', 'España', 'proveedor42@test.org', NULL),
+('C6000063K', 'PROV000063', 'PROVEEDOR 000063', 'Calle Test Num. 783', 'http://test.org', 'España', 'proveedor62@test.org', NULL),
+('C6000073U', 'PROV000073', 'PROVEEDOR 000073', 'Calle Test Num. 641', 'http://test.org', 'España', 'proveedor72@test.org', NULL),
+('C9000053A', 'PROV000053', 'PROVEEDOR 000053', 'Calle Test Num. 116', 'http://test.org', 'España', 'proveedor52@test.org', NULL),
+('C9000093O', 'PROV000093', 'PROVEEDOR 000093', 'Calle Test Num. 68', 'http://test.org', 'España', 'proveedor92@test.org', NULL),
+('D1000044R', 'PROV000044', 'PROVEEDOR 000044', 'Calle Test Num. 967', 'http://test.org', 'España', 'proveedor43@test.org', NULL),
+('D1000074V', 'PROV000074', 'PROVEEDOR 000074', 'Calle Test Num. 172', 'http://test.org', 'España', 'proveedor73@test.org', NULL),
+('D2000034H', 'PROV000034', 'PROVEEDOR 000034', 'Calle Test Num. 543', 'http://test.org', 'España', 'proveedor33@test.org', NULL),
+('D3000014N', 'PROV000014', 'PROVEEDOR 000014', 'Calle Test Num. 338', 'http://test.org', 'España', 'proveedor13@test.org', NULL),
+('D3000024X', 'PROV000024', 'PROVEEDOR 000024', 'Calle Test Num. 205', 'http://test.org', 'España', 'proveedor23@test.org', NULL),
+('D3000064L', 'PROV000064', 'PROVEEDOR 000064', 'Calle Test Num. 764', 'http://test.org', 'España', 'proveedor63@test.org', NULL),
+('D3000094P', 'PROV000094', 'PROVEEDOR 000094', 'Calle Test Num. 507', 'http://test.org', 'España', 'proveedor93@test.org', NULL),
+('D7000084F', 'PROV000084', 'PROVEEDOR 000084', 'Calle Test Num. 51', 'http://test.org', 'España', 'proveedor83@test.org', NULL),
+('D9000004D', 'PROV000004', 'PROVEEDOR 000004', 'Calle Test Num. 822', 'http://test.org', 'España', 'proveedor3@test.org', NULL),
+('D9000054B', 'PROV000054', 'PROVEEDOR 000054', 'Calle Test Num. 168', 'http://test.org', 'España', 'proveedor53@test.org', NULL),
+('E1000025Y', 'PROV000025', 'PROVEEDOR 000025', 'Calle Test Num. 82', 'http://test.org', 'España', 'proveedor24@test.org', NULL),
+('E1000035I', 'PROV000035', 'PROVEEDOR 000035', 'Calle Test Num. 11', 'http://test.org', 'España', 'proveedor34@test.org', NULL),
+('E1000055C', 'PROV000055', 'PROVEEDOR 000055', 'Calle Test Num. 987', 'http://test.org', 'España', 'proveedor54@test.org', NULL),
+('E1000085G', 'PROV000085', 'PROVEEDOR 000085', 'Calle Test Num. 583', 'http://test.org', 'España', 'proveedor84@test.org', NULL),
+('E2000005E', 'PROV000005', 'PROVEEDOR 000005', 'Calle Test Num. 676', 'http://test.org', 'España', 'proveedor4@test.org', NULL),
+('E5000065M', 'PROV000065', 'PROVEEDOR 000065', 'Calle Test Num. 633', 'http://test.org', 'España', 'proveedor64@test.org', NULL),
+('E5000095Q', 'PROV000095', 'PROVEEDOR 000095', 'Calle Test Num. 910', 'http://test.org', 'España', 'proveedor94@test.org', NULL),
+('E6000015O', 'PROV000015', 'PROVEEDOR 000015', 'Calle Test Num. 95', 'http://test.org', 'España', 'proveedor14@test.org', NULL),
+('E7000045S', 'PROV000045', 'PROVEEDOR 000045', 'Calle Test Num. 323', 'http://test.org', 'España', 'proveedor44@test.org', NULL),
+('E8000075W', 'PROV000075', 'PROVEEDOR 000075', 'Calle Test Num. 699', 'http://test.org', 'España', 'proveedor74@test.org', NULL),
+('F1000036J', 'PROV000036', 'PROVEEDOR 000036', 'Calle Test Num. 413', 'http://test.org', 'España', 'proveedor35@test.org', NULL),
+('F2000026Z', 'PROV000026', 'PROVEEDOR 000026', 'Calle Test Num. 109', 'http://test.org', 'España', 'proveedor25@test.org', NULL),
+('F2000066N', 'PROV000066', 'PROVEEDOR 000066', 'Calle Test Num. 373', 'http://test.org', 'España', 'proveedor65@test.org', NULL),
+('F3000086H', 'PROV000086', 'PROVEEDOR 000086', 'Calle Test Num. 590', 'http://test.org', 'España', 'proveedor85@test.org', NULL),
+('F4000016P', 'PROV000016', 'PROVEEDOR 000016', 'Calle Test Num. 151', 'http://test.org', 'España', 'proveedor15@test.org', NULL),
+('F6000076X', 'PROV000076', 'PROVEEDOR 000076', 'Calle Test Num. 5', 'http://test.org', 'España', 'proveedor75@test.org', NULL),
+('F9000006F', 'PROV000006', 'PROVEEDOR 000006', 'Calle Test Num. 91', 'http://test.org', 'España', 'proveedor5@test.org', NULL),
+('F9000046T', 'PROV000046', 'PROVEEDOR 000046', 'Calle Test Num. 484', 'http://test.org', 'España', 'proveedor45@test.org', NULL),
+('F9000056D', 'PROV000056', 'PROVEEDOR 000056', 'Calle Test Num. 746', 'http://test.org', 'España', 'proveedor55@test.org', NULL),
+('F9000096R', 'PROV000096', 'PROVEEDOR 000096', 'Calle Test Num. 506', 'http://test.org', 'España', 'proveedor95@test.org', NULL),
+('G1000027A', 'PROV000027', 'PROVEEDOR 000027', 'Calle Test Num. 234', 'http://test.org', 'España', 'proveedor26@test.org', NULL),
+('G1000057E', 'PROV000057', 'PROVEEDOR 000057', 'Calle Test Num. 296', 'http://test.org', 'España', 'proveedor56@test.org', NULL),
+('G2000087I', 'PROV000087', 'PROVEEDOR 000087', 'Calle Test Num. 315', 'http://test.org', 'España', 'proveedor86@test.org', NULL),
+('G3000067O', 'PROV000067', 'PROVEEDOR 000067', 'Calle Test Num. 229', 'http://test.org', 'España', 'proveedor66@test.org', NULL),
+('G3000097S', 'PROV000097', 'PROVEEDOR 000097', 'Calle Test Num. 623', 'http://test.org', 'España', 'proveedor96@test.org', NULL),
+('G4000017Q', 'PROV000017', 'PROVEEDOR 000017', 'Calle Test Num. 945', 'http://test.org', 'España', 'proveedor16@test.org', NULL),
+('G5000007G', 'PROV000007', 'PROVEEDOR 000007', 'Calle Test Num. 634', 'http://test.org', 'España', 'proveedor6@test.org', NULL),
+('G5000037K', 'PROV000037', 'PROVEEDOR 000037', 'Calle Test Num. 600', 'http://test.org', 'España', 'proveedor36@test.org', NULL),
+('G7000077Y', 'PROV000077', 'PROVEEDOR 000077', 'Calle Test Num. 634', 'http://test.org', 'España', 'proveedor76@test.org', NULL),
+('G9000047U', 'PROV000047', 'PROVEEDOR 000047', 'Calle Test Num. 359', 'http://test.org', 'España', 'proveedor46@test.org', NULL),
+('H2000028B', 'PROV000028', 'PROVEEDOR 000028', 'Calle Test Num. 484', 'http://test.org', 'España', 'proveedor27@test.org', NULL),
+('H2000058F', 'PROV000058', 'PROVEEDOR 000058', 'Calle Test Num. 799', 'http://test.org', 'España', 'proveedor57@test.org', NULL),
+('H2000088J', 'PROV000088', 'PROVEEDOR 000088', 'Calle Test Num. 571', 'http://test.org', 'España', 'proveedor87@test.org', NULL),
+('H3000008H', 'PROV000008', 'PROVEEDOR 000008', 'Calle Test Num. 873', 'http://test.org', 'España', 'proveedor7@test.org', NULL),
+('H3000048V', 'PROV000048', 'PROVEEDOR 000048', 'Calle Test Num. 911', 'http://test.org', 'España', 'proveedor47@test.org', NULL),
+('H5000068P', 'PROV000068', 'PROVEEDOR 000068', 'Calle Test Num. 699', 'http://test.org', 'España', 'proveedor67@test.org', NULL),
+('H7000078Z', 'PROV000078', 'PROVEEDOR 000078', 'Calle Test Num. 253', 'http://test.org', 'España', 'proveedor77@test.org', NULL),
+('H8000098T', 'PROV000098', 'PROVEEDOR 000098', 'Calle Test Num. 520', 'http://test.org', 'España', 'proveedor97@test.org', NULL),
+('H9000018R', 'PROV000018', 'PROVEEDOR 000018', 'Calle Test Num. 641', 'http://test.org', 'España', 'proveedor17@test.org', NULL),
+('H9000038L', 'PROV000038', 'PROVEEDOR 000038', 'Calle Test Num. 487', 'http://test.org', 'España', 'proveedor37@test.org', NULL),
+('I1000009I', 'PROV000009', 'PROVEEDOR 000009', 'Calle Test Num. 421', 'http://test.org', 'España', 'proveedor8@test.org', NULL),
+('I2000089K', 'PROV000089', 'PROVEEDOR 000089', 'Calle Test Num. 743', 'http://test.org', 'España', 'proveedor88@test.org', NULL),
+('I3000019S', 'PROV000019', 'PROVEEDOR 000019', 'Calle Test Num. 790', 'http://test.org', 'España', 'proveedor18@test.org', NULL),
+('I3000029C', 'PROV000029', 'PROVEEDOR 000029', 'Calle Test Num. 499', 'http://test.org', 'España', 'proveedor28@test.org', NULL),
+('I3000039M', 'PROV000039', 'PROVEEDOR 000039', 'Calle Test Num. 142', 'http://test.org', 'España', 'proveedor38@test.org', NULL),
+('I4000099U', 'PROV000099', 'PROVEEDOR 000099', 'Calle Test Num. 540', 'http://test.org', 'España', 'proveedor98@test.org', NULL),
+('I6000049W', 'PROV000049', 'PROVEEDOR 000049', 'Calle Test Num. 332', 'http://test.org', 'España', 'proveedor48@test.org', NULL),
+('I7000079A', 'PROV000079', 'PROVEEDOR 000079', 'Calle Test Num. 533', 'http://test.org', 'España', 'proveedor78@test.org', NULL),
+('I8000059G', 'PROV000059', 'PROVEEDOR 000059', 'Calle Test Num. 114', 'http://test.org', 'España', 'proveedor58@test.org', NULL),
+('I8000069Q', 'PROV000069', 'PROVEEDOR 000069', 'Calle Test Num. 850', 'http://test.org', 'España', 'proveedor68@test.org', NULL),
+('J1000060H', 'PROV000060', 'PROVEEDOR 000060', 'Calle Test Num. 721', 'http://test.org', 'España', 'proveedor59@test.org', NULL),
+('J2000030D', 'PROV000030', 'PROVEEDOR 000030', 'Calle Test Num. 644', 'http://test.org', 'España', 'proveedor29@test.org', NULL),
+('J2000050X', 'PROV000050', 'PROVEEDOR 000050', 'Calle Test Num. 54', 'http://test.org', 'España', 'proveedor49@test.org', NULL),
+('J2000080B', 'PROV000080', 'PROVEEDOR 000080', 'Calle Test Num. 296', 'http://test.org', 'España', 'proveedor79@test.org', NULL),
+('J3000090L', 'PROV000090', 'PROVEEDOR 000090', 'Calle Test Num. 847', 'http://test.org', 'España', 'proveedor89@test.org', NULL),
+('J6000040N', 'PROV000040', 'PROVEEDOR 000040', 'Calle Test Num. 673', 'http://test.org', 'España', 'proveedor39@test.org', NULL),
+('J7000020T', 'PROV000020', 'PROVEEDOR 000020', 'Calle Test Num. 689', 'http://test.org', 'España', 'proveedor19@test.org', NULL),
+('J7000070R', 'PROV000070', 'PROVEEDOR 000070', 'Calle Test Num. 489', 'http://test.org', 'España', 'proveedor69@test.org', NULL),
+('J8000010J', 'PROV000010', 'PROVEEDOR 000010', 'Calle Test Num. 263', 'http://test.org', 'España', 'proveedor9@test.org', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
 --
 
 CREATE TABLE `usuario` (
@@ -83,7 +223,7 @@ CREATE TABLE `usuario` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- A extraer os datos da táboa `usuario`
+-- Volcado de datos para la tabla `usuario`
 --
 
 INSERT INTO `usuario` (`username`, `rol`, `salarioBruto`, `retencionIRPF`, `activo`) VALUES
@@ -182,50 +322,56 @@ INSERT INTO `usuario` (`username`, `rol`, `salarioBruto`, `retencionIRPF`, `acti
 ('Nuria_Maria_Montes_Pereira', 'standard', 2716.00, 18.00, b'1');
 
 --
--- Indexes for dumped tables
+-- Índices para tablas volcadas
 --
 
 --
--- Indexes for table `categoria`
+-- Indices de la tabla `categoria`
 --
 ALTER TABLE `categoria`
   ADD PRIMARY KEY (`id_categoria`),
   ADD KEY `FK_PADRE_CATEGORIA` (`id_padre`);
 
 --
--- Indexes for table `log`
+-- Indices de la tabla `log`
 --
 ALTER TABLE `log`
   ADD PRIMARY KEY (`id_log`,`operacion`,`tabla`);
 
 --
--- Indexes for table `usuario`
+-- Indices de la tabla `proveedor`
+--
+ALTER TABLE `proveedor`
+  ADD PRIMARY KEY (`cif`);
+
+--
+-- Indices de la tabla `usuario`
 --
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`username`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT for table `categoria`
+-- AUTO_INCREMENT de la tabla `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
--- AUTO_INCREMENT for table `log`
+-- AUTO_INCREMENT de la tabla `log`
 --
 ALTER TABLE `log`
   MODIFY `id_log` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- Restricións para os envorcados das táboas
+-- Restricciones para tablas volcadas
 --
 
 --
--- Restricións para a táboa `categoria`
+-- Filtros para la tabla `categoria`
 --
 ALTER TABLE `categoria`
   ADD CONSTRAINT `FK_PADRE_CATEGORIA` FOREIGN KEY (`id_padre`) REFERENCES `categoria` (`id_categoria`) ON UPDATE CASCADE;
